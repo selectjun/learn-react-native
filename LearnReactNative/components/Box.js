2f8daba0508711f547186c7ac29b3d06ced27117
selectjun <selectjun@digital-x.kr>
2021-11-20 15:46:47 +0900

    스타일 커스텀.

--- LearnReactNative/components/Box.js
+++ LearnReactNative/components/Box.js
@@ -2,8 +2,24 @@ import React from 'react';
 import {View, StyleSheet} from 'react-native';
 
 function Box(props) {
-  return <View style={[styles.box, props.rounded ? styles.rounded : null]} />;
+  return (
+    <View
+      style={[
+        styles.box,
+        props.rounded && styles.rounded,
+        sizes[props.size],
+        {
+          backgroundColor: props.color,
+        },
+      ]}
+    />
+  );
 }
+
+Box.defaultProps = {
+  size: 'medium',
+  color: 'black',
+};
 
 const styles = StyleSheet.create({
   box: {
@@ -14,6 +30,24 @@ const styles = StyleSheet.create({
   rounded: {
     borderRadius: 16,
   },
+  small: {
+    width: 32,
+    height: 32,
+  },
+  medium: {
+    width: 64,
+    height: 64,
+  },
+  large: {
+    width: 128,
+    height: 128,
+  },
 });
+
+const sizes = {
+  small: styles.small,
+  medium: styles.medium,
+  large: styles.large,
+};
 
 export default Box;
