0a73068d4f1ddcfdcbf6fda526b8ea7821df8f50
selectjun <selectjun@digital-x.kr>
2021-11-20 15:36:05 +0900

    스타일.

--- LearnReactNative/App.js
+++ LearnReactNative/App.js
@@ -1,16 +1,11 @@
 import React from 'react';
 import {SafeAreaView} from 'react-native';
-import Greeting from './components/Greeting';
+import Box from './components/Box';
 
 const App = () => {
-  const name = 'JSX';
-
   return (
     <SafeAreaView>
-      {/* 주석을 작성해봅시다. */}
-      <Greeting
-        name={name} // 이름을 설정하기
-      />
+      <Box />
     </SafeAreaView>
   );
 };
